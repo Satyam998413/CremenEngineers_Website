5a412a941b4eba0aff5d42eecf917e2ee5847150
Satyam998413 <84256320+Satyam998413@users.noreply.github.com>
2022-02-22 12:41:09 +0530

    This is my all setup djANGO APPS

---
+++ CremenEngineer/CremenEngineer/views.py
@@ -0,0 +1,9 @@
+#i have created this file Satyam
+# from django.http import HttpResponse
+
+# def index(request):
+#     return HttpResponse("Hello")
+
+
+# def about(request):
+#     return HttpResponse("Hello About")
