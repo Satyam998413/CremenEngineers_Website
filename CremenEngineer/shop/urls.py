b0d67a2421a41d8a1b928ab203a1b7193c83f367
Satyam998413 <84256320+Satyam998413@users.noreply.github.com>
2022-02-24 01:02:33 +0530

    Day 2 finally create my aboutus and contact page

--- CremenEngineer/shop/urls.py
+++ CremenEngineer/shop/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path("about",views.about,name="AboutUs"),
     path('contact/', views.contact,name="ContactUs"),
     path('tracker', views.tracker,name="TrackingStatus"),
-  path('search/', views.search,name="Search"),
-    path('productview/', views.productView,name="PoductView"),
+    path('search/', views.search,name="Search"),
+    path('products/<int:myid>', views.productView,name="PoductView"),
     path('checkout/', views.checkout,name="Checkout"),
 
 
--- CremenEngineer/shop/views.py
+++ CremenEngineer/shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from . models import Product
+from . models import Product,Contact
 from math import ceil
 
 def index(request):
@@ -29,10 +29,17 @@ def about(request):
     return render(request,"shop/about.html")
 
 def contact(request):
+    if request.method=="POST":
+        name=request.POST.get('name', '')
+        email=request.POST.get('email', '')
+        phone=request.POST.get('phone', '')
+        desc=request.POST.get('desc', '')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc)
+        contact.save()
     return render(request,"shop/contact.html")
 
 def tracker(request):
-    return render(request,"shop/tacker.html")
+    return render(request,"shop/tracker.html")
 
 def about(request):
    return render(request,"shop/about.html")
@@ -40,8 +47,10 @@ def about(request):
 def search(request):
   return render(request,"shop/search.html")
 
-def productView(request):
-    return render(request,"shop/productview.html")
+def productView(request,myid):
+    #Fetch the product using the id
+    product=Product.objects.filter(id=myid)
+    return render(request,"shop/productview.html", {'product':product[0]})
 
 def checkout(request):
  return render(request,"shop/checkout.html")
