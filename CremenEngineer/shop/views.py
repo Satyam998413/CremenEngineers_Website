ce9af7a6085e6a3b78a2702aa9518774ffd26cf8
Satyam998413 <84256320+Satyam998413@users.noreply.github.com>
2022-02-24 15:06:50 +0530

    Day 2 finally create my aboutus and checkout  page

--- CremenEngineer/shop/views.py
+++ CremenEngineer/shop/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from . models import Product,Contact
+from .models import Product, Contact, Orders
 from math import ceil
+# import the logging library
+import logging
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+# Create your views here.
+from django.http import HttpResponse
 
 def index(request):
     # products = Product.objects.all()
@@ -23,34 +29,50 @@ def index(request):
     #             [products, range(1, nSlides), nSlides]]
     params = {'allProds':allProds}
     return render(request, 'shop/index.html', params)
-   
 
 def about(request):
-    return render(request,"shop/about.html")
+    return render(request, 'shop/about.html')
 
 def contact(request):
     if request.method=="POST":
-        name=request.POST.get('name', '')
-        email=request.POST.get('email', '')
-        phone=request.POST.get('phone', '')
-        desc=request.POST.get('desc', '')
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        desc = request.POST.get('desc', '')
         contact = Contact(name=name, email=email, phone=phone, desc=desc)
         contact.save()
-    return render(request,"shop/contact.html")
+        thank = True
+        name = contact.name
+        return render(request, 'shop/contact.html', {'thank':thank, 'name': name})
+    return render(request, 'shop/contact.html')
 
 def tracker(request):
-    return render(request,"shop/tracker.html")
-
-def about(request):
-   return render(request,"shop/about.html")
+    return render(request, 'shop/tracker.html')
 
 def search(request):
-  return render(request,"shop/search.html")
+    return render(request, 'shop/search.html')
 
-def productView(request,myid):
-    #Fetch the product using the id
-    product=Product.objects.filter(id=myid)
-    return render(request,"shop/productview.html", {'product':product[0]})
+def productView(request, myid):
+    # Fetch the product using the id
+    product = Product.objects.filter(id=myid)
+
+
+    return render(request, 'shop/prodView.html', {'product':product[0]})
 
 def checkout(request):
- return render(request,"shop/checkout.html")
+    if request.method=="POST":
+        items_json = request.POST.get('itemsJson', '')
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        address = request.POST.get('address1', '') + " " + request.POST.get('address2', '')
+        city = request.POST.get('city', '')
+        state = request.POST.get('state', '')
+        zip_code = request.POST.get('zip_code', '')
+        phone = request.POST.get('phone', '')
+        order = Orders(items_json=items_json, name=name, email=email, address=address, city=city,
+                       state=state, zip_code=zip_code, phone=phone)
+        order.save()
+        thank = True
+        id = order.order_id
+        return render(request, 'shop/checkout.html', {'thank':thank, 'id': id})
+    return render(request, 'shop/checkout.html')
