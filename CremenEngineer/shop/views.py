9dfae5ce1e58ab3f08133c29a8662a0787f459ad
Satyam998413 <84256320+Satyam998413@users.noreply.github.com>
2022-02-28 11:23:25 +0530

    Day 5 finally iam created my payment gateway
    uiktguk

--- CremenEngineer/shop/views.py
+++ CremenEngineer/shop/views.py
@@ -6,7 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 from .PayTm import Checksum
 # Create your views here.
 from django.http import HttpResponse
-MERCHANT_KEY = 'Your-Merchant-Key-Here'
+MERCHANT_KEY = 'ho@ByrlVgT6wfbV&'
 
 def index(request):
     allProds = []
@@ -20,6 +20,29 @@ def index(request):
     params = {'allProds':allProds}
     return render(request, 'shop/index.html', params)
 
+
+def searchMatch(query, item):
+    # if query in item.product_name or query in item.category or item.sub_category:
+    #     return True
+    # else:
+        return False
+
+def search(request):
+    query= request.GET.get('search')
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prodtemp = Product.objects.filter(category=cat)
+        prod=[item for item in prodtemp if searchMatch(query, item)]
+    #     n = len(prod)
+    #     nSlides = n // 4 + ceil((n / 4) - (n // 4))
+    #     if len(prod)!= 0:
+    #         allProds.append([prod, range(1, nSlides), nSlides])
+    # params = {'allProds': allProds, "msg":""}
+    # if len(allProds)==0 or len(query)<4:
+    #     params={'msg':"Please make sure to enter relevant search query"}
+    return render(request, 'shop/index.html', prod)
 
 def about(request):
     return render(request, 'shop/about.html')
@@ -59,8 +82,6 @@ def tracker(request):
     return render(request, 'shop/tracker.html')
 
 
-def search(request):
-    return render(request, 'shop/search.html')
 
 
 def productView(request, myid):
