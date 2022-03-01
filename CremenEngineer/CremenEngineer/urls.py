0ddb32c867d9581a992dc20bad02d5510978296d
Satyam998413 <84256320+Satyam998413@users.noreply.github.com>
2022-03-01 17:50:52 +0530

    kporeyht7po
    dsmgxtFMg[

--- CremenEngineer/CremenEngineer/urls.py
+++ CremenEngineer/CremenEngineer/urls.py
@@ -32,6 +32,7 @@ urlpatterns = [
     # path("about",views.about,name="about")
     path("shop/",include("shop.urls")),
     path("blog/",include("blog.urls")),
+  
     
 
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
